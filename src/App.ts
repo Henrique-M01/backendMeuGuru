4aa71b3c8b05241c8e665b0cf389c8e2d65bb17e
henrique <hickm018@gmail.com>
2022-05-11 05:28:42 -0300

    feat: create error middleware

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
 import userRouter from './router/UserRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use('/users',userRouter);
+app.use(errorMiddleware)
 
 export default app;

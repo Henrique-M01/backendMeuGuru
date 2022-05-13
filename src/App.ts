3e9f922772059cdd5a8fa72af498d3f696d5c3d6
henrique <hickm018@gmail.com>
2022-05-13 01:49:21 -0300

    feat: add cors

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import errorMiddleware from './middlewares/errorMiddleware';
 import userRouter from './router/UserRouter';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/users',userRouter);
 app.use(errorMiddleware)
 
